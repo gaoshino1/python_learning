6fe4afc84dd6feb99ad1e7c62c55cfc9a26d132d
Luyu Guo <740877731@qq.com>
2020-01-02 17:40:00 +0800

    update python_lxf

--- python_lxf/python基础.py
+++ python_lxf/python基础.py
@@ -77,3 +77,71 @@ print('小明的成绩从72到85，成绩提升了: %2.1f%%' % r)
 print('小明的成绩从72到85，成绩提升了: {0:2.1f}%'.format(r))
 
 # 使用List和tuple
+## List
+classmates = ['Michael', 'Bob', 'Tracy']
+print(classmates)
+print("长度:", len(classmates))
+print(classmates[0], classmates[1], classmates[-1])
+# print(classmates[3])  # IndexError: list index out of range
+# print(classmates[-4])  # IndexError: list index out of range
+# list是一个可变的有序表
+classmates.append('Adam')
+print(classmates)
+classmates.insert(1, 'Jack')  # 在指定索引位插入
+print(classmates)
+# pop()删除末尾元素
+classmates.pop()
+print(classmates)
+# pop(index) 删除指定位置的元素
+classmates.pop(1)
+print(classmates)
+# 直接替换
+classmates[1] = 'Sarah'
+print(classmates)
+
+s = ['Python', 'Java', ['asp', 'php'], 'scheme']
+print("长度:", len(s))
+
+
+## Tuple  有序列表，一旦初始化就不能修改
+classmates = ('Michael', 'Bob', 'Tracy')
+print(classmates)
+
+# 初始化空tuple
+t = ()
+# 初始化一个元素的tuple，必须要带逗号，否则被当成数字1，因为规定这种情况下是按小括号计算的
+t = (1,)
+# 虽说不能修改，但是！ 看下面   tuple中的一个元素是list，看似是tuple的元素变了其实变的是list的元素
+# tuple所谓的“不变”是说，tuple的每个元素，指向永远不变
+t = ('a', 'b', ['A', 'B'])
+t[2][0] = 'X'
+t[2][1] = 'Y'
+print(t)
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+print(L[0][0])
+print(L[1][1])
+print(L[2][2])
+
+
+# 条件判断
+age = 20
+if age >= 18:
+    print('adult')
+elif age >= 14:
+    print('teenager')
+else:
+    print('you are just a kid')
+# 只要是非零数值、非空字符串、非空list等，就判断为true，否则为false
+
+# input
+birth = input('birth: ')
+birth = int(birth)
+if birth < 2000:  # TypeError: '<' not supported between instances of 'str' and 'int'
+    print('90后')
+else:
+    print('00后')
